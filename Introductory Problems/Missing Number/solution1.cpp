771a4a9dc8c6cce45f5bc2195f9dad72192e2818
Tanasescu Andrei <artanfast@gmail.com>
2023-12-17 22:12:30 +0200

    Create solution1.cpp

---
+++ Introductory Problems/Missing Number/solution1.cpp
@@ -0,0 +1,23 @@
+//solved using a frequency array
+
+#include <iostream>
+
+using namespace std;
+const int Nmax=2e5+5;
+
+int n;
+bool fr[Nmax];
+
+int main(){
+    cin>>n;
+    for (int i=0; i<n; i++){
+        int nr;
+        cin>>nr;
+        fr[nr]=1;
+    }
+    for (int i=1; i<=n; i++)
+        if (!fr[i])
+            cout<<i;
+
+    return 0;
+}
