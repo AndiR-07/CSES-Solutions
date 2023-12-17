25dfced0ed3f38fb3594ebcabdd003e0579d1fe8
Tanasescu Andrei <artanfast@gmail.com>
2023-12-17 22:16:03 +0200

    Create solution2.cpp

---
+++ Introductory Problems/Missing Number/solution2.cpp
@@ -0,0 +1,22 @@
+// solved using Gauss sum formula
+#include <iostream>
+
+using namespace std;
+typedef long long ll;
+const int Nmax=2e5+5;
+
+int n;
+ll s, sg;
+
+int main(){
+    cin>>n;
+    sg=(ll)n*(n+1)/2;
+    for (int i=1; i<n; i++){
+        int nr;
+        cin>>nr;
+        s+=nr;
+    }
+    cout<<sg-s;
+
+    return 0;
+}
